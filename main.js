3f38ee7690014703a9da3ebf980b320358769265
zhrbzd <zhrbbzd@gmail.com>
2023-03-14 00:30:36 +0330

    next and prev buttons

--- main.js
+++ main.js
@@ -1,20 +1,38 @@
-const formOne = document.querySelector("#form_one")
-const formTwo = document.querySelector("#form_two")
-const formThree = document.querySelector("#form_three")
-const step_one = document.querySelector("#step-one")
-const step_two = document.querySelector("#step-two")
-const show_btn = document.querySelector("#show")
-show_btn.addEventListener("click", e => {
-    formOne.style.display = "block";
-})
-function next(){
-    step_one.addEventListener("click", e => {
-        formOne.style.display = "none"
-        formTwo.style.display = "block"
-    })
-}
+const show_btn = document.querySelector("#show"); // declare button that shows modal
 
-function two_step(){
-    formTwo.style.display = "none"
-    formThree.style.display = "block"
-}
+// show button settings
+show_btn.addEventListener("click", e => {
+    form_one.style.display = "block";
+})
+// step 1
+const form_one = document.querySelector("#form-one"); // form 1
+const go_step_two = document.querySelector("#go-step-two");
+go_step_two.addEventListener("click", e => {
+    e.preventDefault()
+    form_one.style.display = "none";
+    form_two.style.display = "block";
+})
+
+// step 2
+const form_two = document.querySelector("#form-two"); // form 2
+const go_step_three = document.querySelector("#go-step-three");
+const return_step_one = document.querySelector("#return-step-one");
+go_step_three.addEventListener("click", e => {
+    e.preventDefault()
+    form_two.style.display = "none";
+    form_three.style.display = "block";
+})
+return_step_one.addEventListener("click", e => {
+    e.preventDefault()
+    form_two.style.display = "none"
+    form_one.style.display = "block"
+})
+
+// step 3
+const form_three = document.querySelector("#form-three") // form 3
+const return_step_two = document.querySelector("#return-step-two");
+return_step_two.addEventListener("click", e => {
+    e.preventDefault()
+    form_three.style.display = "none";
+    form_two.style.display = "block";
+})
